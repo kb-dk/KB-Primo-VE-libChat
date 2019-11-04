1591ee054f6d173a68dc3f45a651afe893d9454e
zahra <azr41432>
2019-11-04 02:17:13 -0800

    Changed chatbox id

--- src/chatbox/scriptIds.component.js
+++ src/chatbox/scriptIds.component.js
@@ -1,4 +1,4 @@
 export const scriptIds = {
-    'en': '2065a8d15fb45f3c911c2b223cc81286',
-    'da': '7df867c6243394f970f8550332c4b607'
+    'en': 'b19d8ae41c784590fd84ba51114c29d2',
+    'da': '668df01b6f2cd836279a993a8fc944ec'
 };
